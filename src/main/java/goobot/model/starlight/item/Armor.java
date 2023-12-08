d8f7ada94730570330226e1ef65e43a6dae4e931
Lukas-Adkins <googmdev@gmail.com>
2023-12-08 11:03:15 -0800

    Updated Armor and Cybernetics with better comments

--- src/main/java/goobot/model/starlight/item/Armor.java
+++ src/main/java/goobot/model/starlight/item/Armor.java
@@ -8,11 +8,29 @@ package goobot.model.starlight.item;
 import goobot.Constants.ItemType;
 import goobot.Constants.Rarity;
 
+/**
+ * Represents an armor item.
+ */
 public class Armor extends Item {
+    private static final String ANSI_FORMAT = "```ansi\n%s / %s / %d CYD\nCovers: %s / AP: %s / Max Agility: %s\n%s```";
+
     private String covers;
     private String AP;
     private String maxAgility;
 
+    /**
+     * Constructs a new Armor object with the specified attributes.
+     *
+     * @param type The type of armor.
+     * @param name The name of the armor.
+     * @param rarity The rarity of the armor.
+     * @param description A description of the armor.
+     * @param weight The weight of the armor.
+     * @param price The price of the armor.
+     * @param covers The coverage provided by the armor.
+     * @param AP The armor points (AP) of the armor.
+     * @param maxAgility The maximum agility supported by the armor.
+    */
     public Armor(ItemType type, String name, Rarity rarity, String description, String weight, Integer price, String covers, String AP, String maxAgility) {
         super(type, name, rarity, description, weight, price);
         this.covers = covers;
@@ -20,33 +38,81 @@ public class Armor extends Item {
         this.maxAgility = maxAgility;
     }
 
+    /**
+     * Gets the coverage provided by the armor.
+     *
+     * @return The coverage of the armor.
+     */
     public String getCovers() {
         return this.covers;
     }
 
+    /**
+     * Sets the coverage provided by the armor.
+     *
+     * @param covers The new coverage value to set.
+     */
     public void setCovers(String covers) {
         this.covers = covers;
     }
 
+    /**
+     * Gets the armor points (AP) of the armor.
+     *
+     * @return The armor points of the armor.
+     */
     public String getAP() {
         return this.AP;
     }
 
+    /**
+     * Sets the armor points (AP) of the armor.
+     *
+     * @param AP The new armor points value to set.
+     */
     public void setAP(String AP) {
         this.AP = AP;
     }
 
+    /**
+     * Gets the maximum agility supported by the armor.
+     *
+     * @return The maximum agility of the armor.
+     */
     public String getMaxAgility() {
         return this.maxAgility;
     }
 
+    /**
+     * Sets the maximum agility supported by the armor.
+     *
+     * @param maxAgility The new maximum agility value to set.
+     */
     public void setMaxAgility(String maxAgility) {
         this.maxAgility = maxAgility;
     }
 
-    @Override
-    public String toString(){
-        return String.format("```ansi\n%s / %s / %d CYD\nCovers: %s / AP: %s / Max Agility: %s\n%s```", getName(), getFormattedRarity(), getPrice(), covers , AP, maxAgility, getDescription());
-    }
 
+    /**
+     * Returns a formatted string representation of the object suitable for displaying in Discord.
+     * The string is formatted as an ANSI code block and includes information about the object's name,
+     * rarity, price, covers, armor points (AP), maximum agility, and description.
+     *
+     * The format is as follows:
+     * ```
+     * ansi
+     * {name} / {formattedRarity} / {price} CYD
+     * Covers: {covers} / AP: {AP} / Max Agility: {maxAgility}
+     * {description}
+     * ```
+     *
+     * If any of the properties (name, formattedRarity, description, etc.) is null, an empty string is used
+     * to prevent NullPointerException in the formatted output.
+     *
+     * @return A formatted string suitable for displaying in Discord.
+     */
+    @Override
+    public String toString() {
+        return String.format(ANSI_FORMAT, getName(), getFormattedRarity(), getPrice(), covers, AP, maxAgility, getDescription() != null ? getDescription() : "");
+    }
 }
--- src/main/java/goobot/model/starlight/item/Cybernetic.java
+++ src/main/java/goobot/model/starlight/item/Cybernetic.java
@@ -8,24 +8,56 @@ package goobot.model.starlight.item;
 import goobot.Constants.Rarity;
 import goobot.Constants.ItemType;
 
+/**
+ * Represents a cybernetic item.
+ */
 public class Cybernetic extends Item {
+    private static final String ANSI_FORMAT = "```ansi\n%s / %s / %s cyber-slots / %d CYD\n%s```";
+
     private String slots;
 
+    /**
+     * Constructs a new Cybernetic object with the specified attributes.
+     *
+     * @param type        The type of cybernetic item.
+     * @param name        The name of the cybernetic item.
+     * @param rarity      The rarity of the cybernetic item.
+     * @param description A description of the cybernetic item.
+     * @param weight      The weight of the cybernetic item.
+     * @param price       The price of the cybernetic item.
+     * @param slots       The number of cyber-slots this item uses.
+     */
     public Cybernetic(ItemType type, String name, Rarity rarity, String description, String weight, Integer price, String slots) {
         super(type, name, rarity, description, weight, price);
         this.slots = slots;
     }
 
+    /**
+     * Gets the number of cyber-slots this item uses.
+     *
+     * @return The number of cyber-slots.
+     */
     public String getSlots() {
         return this.slots;
     }
 
+    /**
+     * Sets the number of cyber-slots available for enhancements.
+     *
+     * @param slots The new number of cyber-slots to set.
+     */
     public void setSlots(String slots) {
         this.slots = slots;
     }
 
+    /**
+     * Returns a formatted string representation of the cybernetic item suitable for displaying in Discord.
+     *
+     * @return A formatted string suitable for displaying in Discord.
+     */
+
     @Override
     public String toString() {
-        return String.format("```ansi\n%s / %s / %s cyber-slots / %d CYD\n%s```", getName(), getFormattedRarity(), slots, getPrice(), getDescription());
+        return String.format(ANSI_FORMAT, getName(), getFormattedRarity(), slots, getPrice(), getDescription() != null ? getDescription() : "");
     }
 }
